fbeabfce4de1c993ea3715d69183eb473c95268c
MadeYoga <vngla21@gmail.com>
2019-01-13 20:54:41 +0630

    update, authenticate username&password

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 def login_view(request):
+    if request.method == 'POST':
+        # submit login button was pressed
+        username = request.POST['username']
+        password = request.POST['pass']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'core/home.html')
+        else:
+            # return invalid login
+            messages.error(request, 'username or password not correct')
     return render(request, 'Login_v4/index.html')
-    
+
 def regis_view(request):
     return render(request, 'Regis_v4/index.html')
