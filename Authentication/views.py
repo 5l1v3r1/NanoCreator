14ed6211c46402a2225eea4c6d8527fde19678c0
MadeYoga <vngla21@gmail.com>
2019-01-15 07:08:37 +0630

    update Authentication Regis views

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 def login_view(request):
@@ -18,4 +19,28 @@ def login_view(request):
     return render(request, 'Login_v4/index.html')
 
 def regis_view(request):
+    if request.method == 'POST':
+        first_name  = request.POST['first_name']
+        last_name   = request.POST['last_name']
+        birthday    = request.POST['birthday']
+        gender      = request.POST['gender']
+        email       = request.POST['email']
+        phone       = request.POST['phone']
+        subject     = request.POST['subject']
+        username    = first_name + " " + last_name
+        password    = 'password'
+        new_user    = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+            )
+        new_user.first_name = first_name
+        new_user.last_name  = last_name
+        new_user.save()
+        login(
+            request,
+            new_user,
+            backend='django.contrib.auth.backends.ModelBackend'
+            )
+        return redirect('home')
     return render(request, 'Regis_v4/index.html')
